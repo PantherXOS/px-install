f2857d2cfdc5240943c8e22e4856f1298cdac1f6
Franz Geffke <franz@pantherx.org>
2023-01-20 22:10:37 +0000

    default to rolling on all channel branches

--- pantherx-on-digitalocean.sh
+++ pantherx-on-digitalocean.sh
@@ -142,7 +142,7 @@ cat >> $CHANNELS <<EOL
 (list (channel
         (name 'guix)
         (url "https://channels.pantherx.org/git/pantherx.git")
-        (branch "rolling-nonlibre"))
+        (branch "rolling"))
       (channel
         (name 'nongnu)
         (url "https://channels.pantherx.org/git/nongnu.git")
